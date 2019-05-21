8848722e6076a30873a5287b7c2024630266950c
Srdan Turudic <srdan.turudic@namics.com>
2019-05-21 17:48:53 +0200

    setup: image, layout, grid, lazyloading, pollyfill object-fit

--- kalex-consulting/src/app/app.module.ts
+++ kalex-consulting/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 import { AppComponent } from './app.component';
+import { KxImageComponent } from './components/atoms/kx-image/kx-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KxImageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LazyLoadImageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
